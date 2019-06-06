120a83ee2011e23ccada95fa8284b823e60390c4
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-06 16:49:59 -0400

    Created NotebookShare table

---
+++ Database/NotebookShare.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[NotebookShare]
+(
+	[User] VARCHAR(50) NOT NULL, 
+    [NotebookId] INT NOT NULL, 
+    [EditPage] BIT NOT NULL, 
+    [AddPage] BIT NOT NULL, 
+    [DeletePage] BIT NOT NULL, 
+    [DateShared] DATE NOT NULL,
+	CONSTRAINT PK_NotebookShare PRIMARY KEY ([User],[NotebookId]), 
+    CONSTRAINT [FK_NotebookShare_User] FOREIGN KEY ([User]) REFERENCES [User]([Email]), 
+    CONSTRAINT [FK_NotebookShare_Notebook] FOREIGN KEY ([NotebookId]) REFERENCES [Notebook]([Id])
+)
