a152e8b533da9ca296c030737cb89f4c446322c7
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-06 16:22:29 -0400

    Created Notebook table

---
+++ Database/Notebook.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Notebook]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [User] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Color] VARCHAR(50) NOT NULL, 
+    [Public] BIT NOT NULL,
+	CONSTRAINT [FK_Notebook_User] FOREIGN KEY ([User]) REFERENCES [User]([Email])
+)
