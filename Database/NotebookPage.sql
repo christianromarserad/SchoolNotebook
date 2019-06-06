35fc6f8ece992f24476797c8acdea7d555d1b2df
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-06 16:39:34 -0400

    Created NotebookPage table

---
+++ Database/NotebookPage.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[NotebookPage]
+(
+	[NotebookId] INT NOT NULL, 
+    [PageNumber] INT NOT NULL, 
+    [Notes] VARCHAR(MAX) NOT NULL,
+	CONSTRAINT PK_NotebookPage PRIMARY KEY ([NotebookId],[PageNumber]), 
+    CONSTRAINT [FK_NotebookPage_NotebookId] FOREIGN KEY ([NotebookId]) REFERENCES [Notebook]([Id]), 
+)
--- Database/NotebookRate.sql
+++ Database/NotebookRate.sql
@@ -3,6 +3,7 @@
 	[User] VARCHAR(50) NOT NULL , 
     [NotebookId] INT NOT NULL, 
     [Rate] INT NOT NULL,
+	CONSTRAINT PK_NotebookRate PRIMARY KEY ([User],[NotebookId]), 
     CONSTRAINT [FK_NotebookRate_User] FOREIGN KEY ([User]) REFERENCES [User]([Email]), 
-    CONSTRAINT [FK_NotebookRate_NotebookId] FOREIGN KEY ([NotebookId]) REFERENCES [Notebook]([Id])
+    CONSTRAINT [FK_NotebookRate_Notebook] FOREIGN KEY ([NotebookId]) REFERENCES [Notebook]([Id])
 )
