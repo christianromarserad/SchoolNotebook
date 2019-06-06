de3ae25bfdc806f680a53fa7c811e72fbdfb6520
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-06 16:30:10 -0400

    Created NotebookRate table

---
+++ Database/NotebookRate.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[NotebookRate]
+(
+	[User] VARCHAR(50) NOT NULL , 
+    [NotebookId] INT NOT NULL, 
+    [Rate] INT NOT NULL,
+    CONSTRAINT [FK_NotebookRate_User] FOREIGN KEY ([User]) REFERENCES [User]([Email]), 
+    CONSTRAINT [FK_NotebookRate_NotebookId] FOREIGN KEY ([NotebookId]) REFERENCES [Notebook]([Id])
+)
