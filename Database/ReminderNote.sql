ff93ea2c655957f148a0deeae853788b41dccb06
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-06 16:14:01 -0400

    Created ReminderNote table

---
+++ Database/ReminderNote.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ReminderNote]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [User] VARCHAR(50) NOT NULL, 
+    [Notes] VARCHAR(50) NOT NULL
+	CONSTRAINT [FK_ReminderNote_User] FOREIGN KEY ([User]) REFERENCES [User]([Email]), 
+)
