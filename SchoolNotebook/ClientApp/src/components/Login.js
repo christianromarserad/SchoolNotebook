2c338a967a0aa7acf6e43ca8565bdb65036cc429
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-09 12:38:36 -0400

    Added a Login component

---
+++ SchoolNotebook/ClientApp/src/components/Login.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Login Please</h1>
+            </div>
+        );
+    }
+}
+
+
+export default Login;
