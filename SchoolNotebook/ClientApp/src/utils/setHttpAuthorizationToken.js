57c8b50b600d69aacf9846890e7c2045867c5c3d
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-09 14:21:26 -0400

    Add a utility javascript function to set jwt http header

---
+++ SchoolNotebook/ClientApp/src/utils/setHttpAuthorizationToken.js
@@ -0,0 +1,12 @@
+﻿import axios from 'axios';
+
+function setHttpAuthorizationToken(token) {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+    else {
+        delete axios.defaults.headers.common['Authorization'];
+    }
+}
+
+export default setHttpAuthorizationToken;
