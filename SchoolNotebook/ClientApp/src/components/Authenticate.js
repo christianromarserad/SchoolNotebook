64d301af3f83258b32c9a7395b3d0d37dbcdd905
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-09 12:38:55 -0400

    Created an Authenticate component

---
+++ SchoolNotebook/ClientApp/src/components/Authenticate.js
@@ -0,0 +1,25 @@
+﻿import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router'
+
+class Authenticate extends Component {
+    render() {
+        const component = this.props.isAuthenticated ? this.props.children : <Redirect to='/login' />;
+        return (
+            <div>
+                { component }
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: state.user.isAuthenticated
+    };
+}
+
+
+export default connect(mapStateToProps)(Authenticate);
+
+
