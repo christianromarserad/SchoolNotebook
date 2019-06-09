97a8bf5e8dbf3c6e241b47e812905874eb9fa596
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-09 14:21:49 -0400

    Added a login action creator

--- SchoolNotebook/ClientApp/src/store/User.js
+++ SchoolNotebook/ClientApp/src/store/User.js
@@ -1,8 +1,25 @@
-﻿const initialState = {
+﻿import setHttpAuthorizationToken from '../utils/setHttpAuthorizationToken';
+import jwt_decode from 'jwt-decode';
+
+const loginType = 'LOGIN';
+
+const initialState = {
     isAuthenticated: false,
     name: ''
 };
 
+function loginActionCreator(token) {
+    localStorage.setItem('jwtToken', token);
+    setHttpAuthorizationToken(token);
+    let jwtDecoded = jwt_decode(token);
+
+    return { type: loginType, payload: { name: jwtDecoded.username, isAuthenticated: true } };
+}
+
 export const reducer = (state = initialState, action) => {
+    if (action.type === loginType) {
+        return action.payload;
+    }
+
     return state;
 };
