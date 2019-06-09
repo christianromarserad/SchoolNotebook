eecf05df19346fe55d0f05a3fc2e3350508b288e
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-09 12:39:07 -0400

    Update App component

--- SchoolNotebook/ClientApp/src/App.js
+++ SchoolNotebook/ClientApp/src/App.js
@@ -4,11 +4,18 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import Login from './components/Login';
+import Authenticate from './components/Authenticate';
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
+    <div>
+        <Authenticate>
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+            </Layout>
+        </Authenticate>
+        <Route exact path='/login' component={Login} />
+    </div>
 );
