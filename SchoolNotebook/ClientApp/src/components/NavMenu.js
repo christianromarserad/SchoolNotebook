29c90dbf031646ae80c97bb4ac7e4324f7327fe6
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-10 16:14:13 -0400

    Update NavMenu component to logout from the website

--- SchoolNotebook/ClientApp/src/components/NavMenu.js
+++ SchoolNotebook/ClientApp/src/components/NavMenu.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { logoutActionCreator } from '../store/User'
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-export default class NavMenu extends React.Component {
-  constructor (props) {
-    super(props);
+class NavMenu extends React.Component {
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle () {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render () {
     return (
         <AppBar position="static">
@@ -26,9 +17,24 @@ export default class NavMenu extends React.Component {
                 <Typography style={{ flexGrow: 1 }} variant="h6" color="inherit">
                     News
                 </Typography>
-                <Button color="inherit" component={Link} to="/counter">Logout</Button>
+                <Typography style={{ flexGrow: 1 }} variant="h6" color="inherit">
+                    { this.props.name }
+                </Typography>
+                <Button color="inherit" onClick={ this.props.logout }>Logout</Button>
             </Toolbar>
         </AppBar>
     );
   }
 }
+
+function mapStateToProps(state) {
+    return {
+        name: state.user.givenName + ' ' + state.user.familyName
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ logout: logoutActionCreator }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavMenu);
