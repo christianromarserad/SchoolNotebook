3f4b7322217fab7822837f2f9113959dba24e62d
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-12 17:15:37 -0400

    Added the bookmark reducer and actions

---
+++ SchoolNotebook/ClientApp/src/store/Bookmark.js
@@ -0,0 +1,109 @@
+﻿import axios from 'axios';
+
+const getBookmarksType = 'GET_BOOKMARKS';
+const isLoadingType = 'GET_BOOKMARKS_LOADING';
+const updateTextFieldsType = 'UPDATE_BOOKMARK_TEXTFIELDS';
+const createBookmarkType = 'CREATE_BOOKMARK';
+const openBookmarkModalType = 'OPEN_BOOKMARK_MODAL'
+const closeBookmarkModalType = 'OPEN_BOOKMARK_MODAL'
+
+const initialState = {
+    isLoading: true,
+    isModalOpen: false,
+    bookmarks: {},
+    bookmarkForm: {
+        url: '',
+        name: ''
+    }
+};
+
+export function openBookmarkModalActionCreator() {
+    return {
+        type: openBookmarkModalType,
+        payload: { isModalOpen: true }
+    };
+}
+
+export function closeBookmarkModalActionCreator() {
+    return {
+        type: closeBookmarkModalType,
+        payload: {
+            isModalOpen: false,
+            bookmarkForm: {
+                url: '',
+                name: ''
+            }
+        }
+    };
+}
+
+export function getBookmarksActionCreator() {
+    return function (dispatch) {
+        dispatch({ type: isLoadingType, payload: { isLoading: true } });
+
+        axios.get('https://localhost:44388/api/Bookmark').then(function (res) {
+            dispatch({
+                type: getBookmarksType,
+                payload: {
+                    bookmarks: res.data,
+                    isLoading: false
+                }
+            });
+        });
+    }
+}
+
+export function updateTextFieldsActionCreator(event) {
+    return {
+        type: updateTextFieldsType,
+        payload: { [event.target.name]: event.target.value }
+    };
+}
+
+export function createBookmarkActionCreator() {
+    return function (dispatch, getState) {
+        let bookmarkFormData = getState().bookmark.bookmarkForm;
+        axios.post('https://localhost:44388/api/Bookmark', bookmarkFormData).then(function (res) {
+            dispatch(getBookmarksActionCreator());
+            dispatch(closeBookmarkModalActionCreator());
+        });
+    }
+}
+
+export const reducer = (state = initialState, action) => {
+    if (action.type === getBookmarksType) {
+        return {
+            ...state,
+            ...action.payload
+        }
+    }
+    if (action.type === isLoadingType) {
+        return {
+            ...state,
+            ...action.payload
+        }
+    }
+    else if (action.type === updateTextFieldsType) {
+        return {
+            ...state,
+            bookmarkForm: {
+                ...state.bookmarkForm,
+                ...action.payload
+            }
+        }
+    }
+    else if (action.type === openBookmarkModalType) {
+        return {
+            ...state,
+            ...action.payload
+        };
+    }
+    else if (action.type === closeBookmarkModalType) {
+        return {
+            ...state,
+            ...action.payload
+        };
+    }
+
+    return state;
+};
--- SchoolNotebook/ClientApp/src/store/configureStore.js
+++ SchoolNotebook/ClientApp/src/store/configureStore.js
@@ -4,12 +4,14 @@ import { routerReducer, routerMiddleware } from 'react-router-redux';
 import * as Counter from './Counter';
 import * as WeatherForecasts from './WeatherForecasts';
 import * as User from './User';
+import * as Bookmark from './Bookmark';
 
 export default function configureStore (history, initialState) {
   const reducers = {
     counter: Counter.reducer,
     weatherForecasts: WeatherForecasts.reducer,
     user: User.reducer,
+    bookmark: Bookmark.reducer
   };
 
   const middleware = [
