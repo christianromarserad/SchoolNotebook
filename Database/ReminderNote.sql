f0c13b4e9e8576e8a884b3e461807d090a354949
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-13 15:44:36 -0400

    Set identity in ReminderNote table

--- Database/ReminderNote.sql
+++ Database/ReminderNote.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ReminderNote]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [User] VARCHAR(50) NOT NULL, 
     [Notes] VARCHAR(50) NOT NULL,
 	CONSTRAINT [FK_ReminderNote_User] FOREIGN KEY ([User]) REFERENCES [User]([Email])
