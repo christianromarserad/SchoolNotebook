579079a5d5d1ff1c6d63b83219b1cdafbd283852
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-14 16:13:48 -0400

    Created a Notebook component for the home page

--- SchoolNotebook/ClientApp/src/components/Home.js
+++ SchoolNotebook/ClientApp/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Bookmark from './home/Bookmark';
+import ReminderNote from './home/ReminderNote';
+import Notebook from './home/Notebook';
 
 class Home extends Component {
 
@@ -7,6 +9,8 @@ class Home extends Component {
         return (
             <div>
                 <Bookmark />
+                <ReminderNote />
+                <Notebook />
             </div>
         );
     }
---
+++ SchoolNotebook/ClientApp/src/components/home/Notebook.js
@@ -0,0 +1,65 @@
+﻿import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getNotebooksActionCreator, closeNotebookModalActionCreator, openNotebookModalActionCreator, createNotebookActionCreator, updateTextFieldsActionCreator } from '../../store/Notebook';
+
+class Bookmark extends Component {
+
+    componentDidMount() {
+        this.props.getNotebooksActionCreator();
+    }
+
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="primary" onClick={this.props.openNotebookModalActionCreator}>Create Notebook</Button>
+                <div>
+                    {this.props.notebooks.map((item) => {
+                        return <Button variant="contained" color="default">{item.name}</Button>
+                    })}
+                </div>
+                <Dialog open={this.props.isModalOpen} onClose={this.props.closeNotebookModalActionCreator} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Create Notebook</DialogTitle>
+                    <DialogContent>
+                        <TextField margin="normal" label="Name" value={this.props.name} fullWidth onChange={this.props.updateTextFieldsActionCreator} name='name' />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.props.closeNotebookModalActionCreator} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.props.createNotebookActionCreator} color="primary">
+                            Submit
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        notebooks: state.notebook.notebooks,
+        isModalOpen: state.notebook.isModalOpen,
+        name: state.notebook.notebookForm.name,
+        public: state.notebook.notebookForm.public
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        getNotebooksActionCreator: getNotebooksActionCreator,
+        openNotebookModalActionCreator: openNotebookModalActionCreator,
+        closeNotebookModalActionCreator: closeNotebookModalActionCreator,
+        createNotebookActionCreator: createNotebookActionCreator,
+        updateTextFieldsActionCreator: updateTextFieldsActionCreator
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bookmark);
