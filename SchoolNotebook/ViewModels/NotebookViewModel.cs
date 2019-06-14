bb211e2a85872bc9853022599ba6c8f15b1d5795
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-06-14 16:12:30 -0400

    Created a Notebook view model

---
+++ SchoolNotebook/ViewModels/NotebookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolNotebook.ViewModels
+{
+    public class NotebookViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Public { get; set; }
+    }
+}
