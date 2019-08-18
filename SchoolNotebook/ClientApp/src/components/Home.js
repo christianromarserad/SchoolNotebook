4734841589a4cb0ebe6e7de162927bf72c734f43
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-08-18 13:26:44 -0400

    Improved home page UI

--- SchoolNotebook/ClientApp/src/components/Home.js
+++ SchoolNotebook/ClientApp/src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Bookmark from './home/Bookmark';
-import ReminderNote from './home/ReminderNote';
-import Notebook from './home/Notebook';
+import BookmarkHomePage from './Bookmark/BookmarkHomePage';
+import ReminderNoteHomePage from './ReminderNote/ReminderNoteHomePage';
+import NotebookHomePage from './Notebook/NotebookHomePage';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-    home: {
-        margin: 40
+    margin: {
+        margin: '40px'
     }
 };
 
@@ -14,10 +17,18 @@ class Home extends Component {
 
     render() {
         return (
-            <div className={this.props.classes.home}>
-                <Bookmark />
-                <ReminderNote />
-                <Notebook />
+            <div className={this.props.classes.margin}>
+                <Grid container spacing={8}>
+                    <Grid item lg={12} >
+                        <BookmarkHomePage />
+                    </Grid>
+                    <Grid item lg={12} >
+                        <ReminderNoteHomePage />
+                    </Grid>
+                    <Grid item lg={12} >
+                        <NotebookHomePage />
+                    </Grid>
+                </Grid>
             </div>
         );
     }
