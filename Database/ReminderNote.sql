050728fb961262169cacc7e2c3cd245e73eab4d2
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-08-18 13:22:53 -0400

    Extended ReminderNote character capacity

--- Database/ReminderNote.sql
+++ Database/ReminderNote.sql
@@ -2,6 +2,6 @@
 (
 	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [User] VARCHAR(50) NOT NULL, 
-    [Notes] VARCHAR(50) NOT NULL,
+    [Notes] VARCHAR(200) NOT NULL,
 	CONSTRAINT [FK_ReminderNote_User] FOREIGN KEY ([User]) REFERENCES [User]([Email])
 )
