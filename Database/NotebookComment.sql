d471d288d760f9bd25e3accddd79643b6e40a67a
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-09-13 14:33:32 -0400

    Set comment character count to max

--- Database/NotebookComment.sql
+++ Database/NotebookComment.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL IDENTITY, 
     [NotebookId] INT NOT NULL, 
     [User] VARCHAR(50) NOT NULL, 
-    [Comment] VARCHAR(50) NOT NULL, 
+    [Comment] VARCHAR(MAX) NOT NULL, 
     [Date] DATE NOT NULL, 
     CONSTRAINT [PK_NotebookComment] PRIMARY KEY ([Id]),
 	CONSTRAINT [FK_NotebookComment_Notebook] FOREIGN KEY ([NotebookId]) REFERENCES [Notebook]([Id]), 
